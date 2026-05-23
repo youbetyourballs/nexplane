eb7005f4d6e9e2de8c18cef2d402152eb3a490f9
youbetyourballs <john.o.terrill@gmail.com>
2026-05-23 12:20:20 -0400

    fix(migration): merge santa_sync_server branch to resolve multiple heads

---
+++ backend/alembic/versions/014_merge_santa_sync_heads.py
@@ -0,0 +1,20 @@
+"""merge santa_sync_server branch into main
+
+Revision ID: b7c8d9e0f1a2
+Revises: a9b8c7d6e5f4, b994b0b54f7b
+Create Date: 2026-05-23
+"""
+from alembic import op
+
+revision = 'b7c8d9e0f1a2'
+down_revision = ('a9b8c7d6e5f4', 'b994b0b54f7b')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
