fc43008077195c4de31abbf4262f9bd61676b7c8
youbetyourballs <john.o.terrill@gmail.com>
2026-06-02 02:52:09 -0400

    refactor(agent/ebpf): extract toFloat to ebpf_util.go for cross-platform reuse

---
+++ agent/commands/ebpf/ebpf_util.go
@@ -0,0 +1,13 @@
+package ebpf
+
+func toFloat(v any) float64 {
+	switch x := v.(type) {
+	case float64:
+		return x
+	case int:
+		return float64(x)
+	case int64:
+		return float64(x)
+	}
+	return 0
+}
