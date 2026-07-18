67f01961b700d99535df804c18d3ed3ef36ae0e4
youbetyourballs <john.o.terrill@gmail.com>
2026-07-17 22:20:30 -0400

    fix(migration): shorten cred002 revision ID to fit VARCHAR(32)

--- backend/alembic/versions/cred002_credential_rotation_fanout.py
+++ backend/alembic/versions/cred002_credential_rotation_fanout.py
@@ -2,14 +2,14 @@
 # Copyright (C) 2024-2026 Nexplane, Inc.
 """add credential_rotation_fanout change_type
 
-Revision ID: cred002_credential_rotation_fanout
+Revision ID: cred002_cred_rotation_fanout
 Revises: cert003_merge_heads
 Create Date: 2026-07-17
 """
 from typing import Union
 from alembic import op
 
-revision: str = "cred002_credential_rotation_fanout"
+revision: str = "cred002_cred_rotation_fanout"
 down_revision: Union[str, None] = "cert003_merge_heads"
 branch_labels = None
 depends_on = None
